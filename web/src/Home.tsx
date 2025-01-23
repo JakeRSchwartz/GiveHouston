55cc378c20c946765df089d8bff35eede5489cae
JakeRSchwartz <jakerschwartz47@gmail.com>
2025-01-23 17:59:55 -0600

    Navbar started and boilerplate added

---
+++ web/src/Home.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import styled from 'styled-components'
+
+function Home () {
+  return(
+    <Body>Home</Body>
+  )
+}
+
+export default Home
+
+const Body = styled.div`
+background-color: var(--main-bg-blue);
+width: 100%;
+height: 100vh;`
+
+
