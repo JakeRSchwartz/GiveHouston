0ad33599f5ea4e56756d4187a35a43bf93d2437c
JakeRSchwartz <jakerschwartz47@gmail.com>
2025-02-08 14:53:40 -0600

    BodyOne

---
+++ client/src/components/BodyOne.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components'
+import '../styles/body.styles.css'
+import houstonbanner from '../assets/houston-art.jpg'
+
+const BodyOne = () => {
+  return (
+    <div className='body-one'>
+      <BodyOneLeft>
+        <img src={houstonbanner} alt='Houston skyline' />
+      </BodyOneLeft>
+      <BodyOneRight>
+        <HeaderDiv>
+          <SmallHeader>GIVE BACK TO THE COMMUNITY YOU LOVE - GIVE HOUSTON</SmallHeader>
+          <h4 style={{ padding:'0 5rem 0 5rem'} }>
+            Thanks to the generosity of our community, families in Houston never
+            have to worry about the cost of essential services. Whether it’s
+            medical care, housing, food, or support, we ensure that those in
+            need receive help without financial burden—so they can focus on
+            rebuilding their lives and securing a better future.
+          </h4>
+        </HeaderDiv>
+        <RegistrationDiv>
+            <h2>REGISTER NOW</h2>
+    <RegistrationForm>
+
+        </RegistrationForm>
+        </RegistrationDiv>
+
+      </BodyOneRight>
+    </div>
+  )
+}
+
+export default BodyOne
+
+const HeaderDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  margin: 4rem 0 0 0;
+`
+
+const SmallHeader = styled.h2`
+  font-size: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding:0 5rem 0 5rem;
+  margin-bottom:1rem;
+`
+
+const BodyOneLeft = styled.div`
+  width: 40%;
+  border: solid red 1px;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+`
+const BodyOneRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: solid green 1px;
+  align-items: center;
+  width: 60%;
+`
+const RegistrationDiv = styled.div`
+  display: flex;
+    flex-direction: column;
+  border:solid orange 2px;
+  width: 100%;
+  padding: 2rem;
+  & h2 {
+  margin:0 auto 0 auto;}`
+
+  const RegistrationForm = styled.div`
+  width: 100%;
+  background-color:var(--secondary-red);
+  border-radius:.2rem;
+  padding: 2rem;`
