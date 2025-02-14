4652077901a2b8535d5625f2f405ab3051a24bf7
ramperdan42 <ramper.dan42@gmail.com>
2025-02-14 16:22:49 -0600

    Adding changes from the original event creation branch to this one to prevent merge conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import RegistrationPage from './pages/RegistrationPage'
 import Navbar from './components/Navbar'
+import EventCreationPage from './pages/EventCreationPage'
 function App () {
   type role = 'admin' | 'guest' | 'user'
   const userRole: role = 'guest'
@@ -12,6 +12,7 @@ function App () {
         <Route path='/' element={<Navbar userRole={userRole} />}>
           <Route index element={<Home userRole={userRole}/>} />
           <Route path='register' element={<RegistrationPage/>} />
+          <Route path="event" element={<EventCreationPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/EventCreationPage.tsx
@@ -0,0 +1,10 @@
+import EventCreationForm from "../components/user.comp/EventCreationForm"
+import '../styles/registration.styles.css'
+const EventCreationPage = () => {
+  return (
+    <div className="main-regi-div">
+            <EventCreationForm />
+    </div>
+  )
+}
+export default EventCreationPage
